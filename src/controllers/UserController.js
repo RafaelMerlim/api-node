7774f92b4d587f1688e8d416535a808db964a226
Rafael Merlim <rafaelmerlim7@gmail.com>
2020-06-27 23:16:40 -0300

    solved12

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,7 +3,7 @@
 
 // Após criar o model (a tabela) no arquivo model.js precisamos consumir ele aqui para responder o client e salvar os dados na tabela criada.
 // E esta tabela ficará armazenada no banco de dados.
-const User = require("c:/Users/rafam/Documents/1_CURSOS/Desenvolvimento_Web/node/api-node/src/models/User")
+const User = require("c:/Users/rafam/Documents/1_CURSOS/Desenvolvimento_Web/node/api-node/src/models/User.js")
 
 // req e res devem ficar sempre nesta ordem.
 module.exports = {
